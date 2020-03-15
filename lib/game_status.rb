95e7ebd055d76e68c57f6ae2dc60bdfeee9bd4bc
Austin Buhler <austinbuhler@gmail.com>
2020-03-15 17:04:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,8 @@ def won?(board)
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 = "O"
       return win_combo
+    else
+      return false
     end
   end
 end
