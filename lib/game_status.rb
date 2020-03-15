35e9e171bf4c93aad18d90eb9f028d43bba711ce
Austin Buhler <austinbuhler@gmail.com>
2020-03-15 17:03:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,10 +20,11 @@ def won?(board)
     board[win_combo[0]] = position_1
     board[win_combo[1]] = position_2
     board[win_combo[2]] = position_3
-  end
+
     if position_1 == "X" && position_2 == "X" && position_3 = "X"
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 = "O"
       return win_combo
     end
   end
+end
